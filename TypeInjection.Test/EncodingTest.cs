1a131dbdeaec105ed7e4d72d19bbb7647303ae89
Thomas Kägi <thomas.kaegi@betterask.erni>
2023-12-07 15:25:50 +0800

    Add .net skeleton project.

---
+++ TypeInjection.Test/EncodingTest.cs
@@ -0,0 +1,10 @@
+namespace TypeInjection.Test;
+
+public class EncodingTest
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
---
+++ TypeInjection/Encoding.cs
@@ -0,0 +1,6 @@
+﻿namespace TypeInjection;
+
+public class Encoding
+{
+
+}
