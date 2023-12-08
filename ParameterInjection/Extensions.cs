439214ca9fd669967a095f5e0daf35318776c855
Thomas Kägi <thomas.kaegi@betterask.erni>
2023-12-08 16:30:56 +0800

    Have Create example be more symmetric.

--- ParameterInjection/Extensions.cs
+++
@@ -1,6 +0,0 @@
-namespace ParameterInjection;
-
-public static class Extensions
-{
-    internal static Combo With(this Combo combo, IEncoder encoder) => new(combo, encoder);
-}
--- TypeInjection.Benchmark/CreateMultipleEncodings.cs
+++ TypeInjection.Benchmark/CreateMultipleEncodings.cs
@@ -12,7 +12,7 @@ public class CreateMultipleEncodings
     [Benchmark(Baseline = true)]
     public ITextProcessor Parameterised()
     {
-        return new TextProcessor(new Combo(E.HomogeniseNewLines, E.Flatten).With(E.Trim).With(E.LowerCase));
+        return new TextProcessor(new Combo(new Combo(new Combo(E.HomogeniseNewLines, E.Flatten), E.Trim), E.LowerCase));
     }
 
     [Benchmark]
@@ -20,7 +20,6 @@ public class CreateMultipleEncodings
     {
         return Processor.With<Combo<Combo<Combo<HomogeniseNewLines, Flatten>, Trim>, LowerCase>>();
     }
-
 }
 
 /*
@@ -35,6 +34,6 @@ BenchmarkDotNet v0.13.11, Windows 10 (10.0.19045.3693/22H2/2022Update)
 
 | Method        | Mean       | Error     | StdDev    | Ratio | Gen0   | Allocated | Alloc Ratio |
 |-------------- |-----------:|----------:|----------:|------:|-------:|----------:|------------:|
-| Parameterised | 15.9230 ns | 0.1987 ns | 0.1551 ns |  1.00 | 0.0127 |     120 B |        1.00 |
-| TypeInjection |  0.7198 ns | 0.0170 ns | 0.0142 ns |  0.05 |      - |         - |        0.00 |
+| Parameterised | 16.2689 ns | 0.1963 ns | 0.1740 ns |  1.00 | 0.0127 |     120 B |        1.00 |
+| TypeInjection |  0.9014 ns | 0.0367 ns | 0.0306 ns |  0.06 |      - |         - |        0.00 |
 */
